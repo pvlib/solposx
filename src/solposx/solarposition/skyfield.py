6902c325e91e40db59dca45258fbbba2ec8c904d
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2025-08-01 15:28:05 +0200

    Add docuemntation and tests

--- src/solposx/solarposition/skyfield.py
+++ src/solposx/solarposition/skyfield.py
@@ -1,3 +1,4 @@
+"""Calculate solar position using Skyfield."""
 
 from skyfield.api import load, wgs84
 import pandas as pd
@@ -7,7 +8,44 @@ TS = load.timescale()
 
 
 def skyfield(times, latitude, longitude, de=DE440):
+    """
+    Calculate solar position using the Skyfield Python package.
 
+    Skyfield is a Python package that can calculate high precision
+    position of stars, planets, and satellites in orbit around the Earth based
+    on epherides. Calculated positions should agree with the Astronomical
+    Almanac to within 0.0005 arcseconds.
+
+    Parameters
+    ----------
+    times : pandas.DatetimeIndex
+        Time stamps for which to calculate solar position. Must be timezone
+        aware.
+    latitude : float
+        Latitude in decimal degrees. Positive north of equator, negative
+        to south. [degrees]
+    longitude : float
+        Longitude in decimal degrees. Positive east of prime meridian,
+        negative to west. [degrees]
+    de : Skyfield SpiceKernel, optional, default DE440
+        Eempheris of choice.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame with the following columns (all values in degrees):
+
+        - elevation : actual sun elevation (not accounting for refraction).
+        - zenith : actual sun zenith (not accounting for refraction).
+        - azimuth : sun azimuth, east of north.
+
+    References
+    ----------
+    .. [1] Skyfield website
+       https://rhodesmill.org/skyfield/
+    .. [2] Skyfield GitHub repository
+       https://github.com/skyfielders/python-skyfield
+    """
     earth = de['Earth']
     sun = de['Sun']
 
@@ -17,6 +55,7 @@ def skyfield(times, latitude, longitude, de=DE440):
 
     result = pd.DataFrame({
         'elevation': alt.degrees,
+        'zenith': 90 - alt.degrees,
         'azimuth': az.degrees,
     }, index=times)
 
--- tests/test_solarposition.py
+++ tests/test_solarposition.py
@@ -8,6 +8,7 @@ from solposx.solarposition import psa
 from solposx.solarposition import usno
 from solposx.solarposition import sg2
 from solposx.solarposition import sg2_c
+from solposx.solarposition import skyfield
 from solposx.solarposition import spa
 from solposx.solarposition import walraven
 
@@ -323,6 +324,34 @@ def expected_walraven():
     return data
 
 
+def expected_skyfield():
+    columns = ['elevation', 'zenith', 'azimuth']
+    values = [
+        [32.21731177, 57.78268823, 204.91794786],
+        [32.20484556, 57.79515444, 204.96423388],
+        [34.92239511, 55.07760489, 169.37578931],
+        [18.63535617, 71.36464383, 234.18984836],
+        [35.75683037, 54.24316963, 197.66893877],
+        [-9.5305175, 99.5305175, 201.18798691],
+        [66.85755467, 23.14244533, 245.08983994],
+        [9.52569869, 80.47430131, 338.81201317],
+        [50.10684928, 39.89315072, 326.23556156],
+        [35.36142589, 54.63857411, 175.38836795],
+        [-53.24128453, 143.24128453, 18.64711844],
+        [-53.24128453, 143.24128453, 18.64711844],
+        [32.43211642, 57.56788358, 205.06175657],
+        [32.63128589, 57.36871411, 204.24697987],
+        [-23.40856284, 113.40856284, 79.54814898],
+        [1.10723972, 88.89276028, 104.53937506],
+        [32.21731177, 57.78268823, 204.91794786],
+        [32.21731177, 57.78268823, 204.91794786],
+        [32.21731177, 57.78268823, 204.91794786],
+    ]
+    data = pd.DataFrame(data=values, columns=columns)
+    data['zenith'] = 90 - data['elevation']
+    return data
+
+
 def expected_spa():
     columns = ['apparent_zenith', 'zenith', 'apparent_elevation', 'elevation',
                'azimuth', 'equation_of_time']
@@ -428,8 +457,9 @@ def _generate_solarposition_dataframe(inputs, algorithm, **kwargs):
     (usno, expected_usno_option_3, {'gmst_option': 3}),
     (sg2, expected_sg2, {}),
     (sg2_c, expected_sg2, {}),
-    (walraven, expected_walraven, {}),
+    (skyfield, expected_skyfield, {}),
     (spa, expected_spa, {}),
+    (walraven, expected_walraven, {}),
 ])
 def test_algorithm(algorithm, expected, kwargs, test_conditions):
     expected = expected().set_index(test_conditions.index)
