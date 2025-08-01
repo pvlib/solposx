5171f2cea91500f97231ef99891cf829b308d1a4
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2025-08-01 15:57:39 +0200

    Update hughes.py

--- src/solposx/refraction/hughes.py
+++ src/solposx/refraction/hughes.py
@@ -36,11 +36,9 @@ def hughes(elevation, pressure=101325., temperature=12.):
     .. math::
 
         \begin{align}
-        & \text{For }5° < el <= 90°: ref = \frac{58.1}{\text{tan}(el)} - \frac{0.07}{\text{tan}(el)^3} + \frac{8.6\cdot 10^{-5}}{tan(el)^5}\\
-
-        & \text{For }-0.575° < el <= 5°: ref = el \cdot (-518.2 + el \cdot (103.4 + el \cdot (-12.79 + el \cdot 0.711))) + 1735\\
-
-        & \text{For }el <= -0.575°: ref = \frac{-20.774}{\text{tan}(el)}\\
+        & \frac{58.1}{\text{tan}(el)} - \frac{0.07}{\text{tan}(el)^3} + \frac{8.6\cdot 10^{-5}}{tan(el)^5} \text{ for } 5° < el <= 90°\\
+        & el \cdot (-518.2 + el \cdot (103.4 + el \cdot (-12.79 + el \cdot 0.711))) + 1735 \text{ for } -0.575° < el <= 5°\\
+        & \frac{-20.774}{\text{tan}(el)} \text{ for } el <= -0.575°\\
         end{align}
 
     where :math:`el` is the true (unrefracted) solar elevation angle.
