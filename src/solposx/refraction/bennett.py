26f48793e662179d2172ec0c7d1d5e634e5206ec
Echedey Luis Álvarez <80125792+echedey-ls@users.noreply.github.com>
2025-08-13 00:58:47 +0100

    Add noqa line too long to bennet refraction

--- src/solposx/refraction/bennett.py
+++ src/solposx/refraction/bennett.py
@@ -39,7 +39,7 @@ def bennett(elevation, pressure=101325., temperature=12.):
     .. [1] G. Bennett, "The calculation of astronomical refraction in marine
        navigation," Journal of Navigation, vol. 35, issue 2, pp. 255-259,
        1982, :doi:`10.1017/S0373463300022037`.
-    """
+    """  # noqa: E501
 
     pressure = pressure / 100  # convert to hPa
 
