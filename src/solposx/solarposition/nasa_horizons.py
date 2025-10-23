32e4e0af545aa6b9e3bfa0b546c4e1bb01a9f22f
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2025-10-23 23:05:26 +0200

    Add refraction_correction option to nasa_horizons solar position algorithm (#133)
    * Add adminition for nasa_horizons
    
    * Correct admonition formatting
    
    * Fix indentation
    
    * Add refraction_correction option to nasa_horizons solar position algorithm
    
    * (auto) Paper PDF Draft
    
    * Fix doc warning
    
    * Fix rendering
    
    Co-authored-by: Kevin Anderson <kevin.anderso@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ioannis Sifnaios <88548539+IoannisSifnaios@users.noreply.github.com>
    
    * Add refraction algorithm note and reference
    
    * (auto) Paper PDF Draft
    
    ---------
    
    Co-authored-by: AdamRJensen <AdamRJensen@users.noreply.github.com>
    Co-authored-by: Kevin Anderson <kevin.anderso@gmail.com>
    Co-authored-by: Ioannis Sifnaios <88548539+IoannisSifnaios@users.noreply.github.com>

--- src/solposx/solarposition/nasa_horizons.py
+++ src/solposx/solarposition/nasa_horizons.py
@@ -2,12 +2,11 @@ import requests
 import pandas as pd
 import io
 
-URL = "https://ssd.jpl.nasa.gov/api/horizons.api"
+URL = 'https://ssd.jpl.nasa.gov/api/horizons.api'
 
 
-def nasa_horizons(
-    latitude, longitude, start, end, elevation=0.0, *, time_step="1h", url=URL
-):
+def nasa_horizons(latitude, longitude, start, end, elevation=0., *,
+                  time_step='1h', refraction_correction=False, url=URL):
     """
     Retrieve solar positions from NASA's Horizons web service.
 
@@ -42,6 +41,9 @@ def nasa_horizons(
         Time step size of the requested time series. '1m' for minutes,
         '1h' for hours, '1d' for days, '1mo' for months, and '1y' for years.
         The default is '1h'.
+    refraction_correction : bool, optional
+        Whether to return apparent solar position angles corrected for
+        atmospheric refraction. The defalt is False.
     url : str, optional
         API endpoint. The default is
         ``'https://ssd.jpl.nasa.gov/api/horizons.api'``.
@@ -49,16 +51,20 @@ def nasa_horizons(
     Returns
     -------
     pandas.DataFrame
-        DataFrame with the following columns (all values in degrees):
+        DataFrame with the following columns in degrees (note that all columns
+        except azimuth are prefixed with `'apparent_'` if
+        ``refraction_correction=True``.)
 
-        - uncertainty_right_ascension
-        - uncertainty_declination
         - right_ascension
         - declination
-        - apparent_right_ascesion
-        - apparent_declination
-        - apparent_azimuth
-        - apparent_elevation
+        - azimuth
+        - elevation
+
+    Notes
+    -----
+    The refraction correction algorithm is the same as
+    :py:func:`solposx.refraction.spa`. See [3]_ for more
+    information.
 
     References
     ----------
@@ -66,6 +72,8 @@ def nasa_horizons(
        <https://ssd.jpl.nasa.gov/horizons/>`_
     .. [2] `NASA Horizons API
        <https://ssd-api.jpl.nasa.gov/doc/horizons.html>`_
+    .. [3] `NASA Horizons Manual
+       <https://ssd.jpl.nasa.gov/horizons/manual.html>`_
     """
     params = {
         "MAKE_EPHEM": "YES",  # generate ephemeris
@@ -74,8 +82,8 @@ def nasa_horizons(
         "CENTER": "coord@399",  # input coordinates for Earth (399)
         "COORD_TYPE": "GEODETIC",  # latitude, longitude, elevation in degrees
         "SITE_COORD": f"{longitude},{latitude},{elevation/1000}",
-        "START_TIME": pd.Timestamp(start).strftime("%Y-%m-%d %H:%M"),
-        "STOP_TIME": pd.Timestamp(end).strftime("%Y-%m-%d %H:%M"),
+        "START_TIME": pd.Timestamp(start).strftime('%Y-%m-%d %H:%M'),
+        "STOP_TIME": pd.Timestamp(end).strftime('%Y-%m-%d %H:%M'),
         "STEP_SIZE": time_step,
         "QUANTITIES": "2,4",  # corrected angles
         "REF_SYSTEM": "ICRF",
@@ -83,20 +91,25 @@ def nasa_horizons(
         "CAL_TYPE": "MIXED",  # Gregorian or mixed Julian/Gregorian calendar
         "TIME_DIGITS": "FRACSEC",  # output time precision
         "ANG_FORMAT": "DEG",  # output angles in degrees
-        "APPARENT": "AIRLESS",  # no refraction
-        # "RANGE_UNITS": "AU",
-        # "SUPPRESS_RANGE_RATE": "NO",
         "SKIP_DAYLT": "NO",  # include daylight periods
         "SOLAR_ELONG": "0,180",
         "EXTRA_PREC": "NO",  # toggle additional digits on some angles (RA/DEC)
         "CSV_FORMAT": "NO",
-        "OBJ_DATA": "NO",  # whether to return summary data
+        "OBJ_DATA": "NO"  # whether to return summary data
     }
 
+    if refraction_correction:
+        params["APPARENT"] = "REFRACTED"  # account for refraction
+    else:
+        params["APPARENT"] = "AIRLESS"  # do not account for refraction
+
     # manual formatting of the url as all parameters except format shall be
-    # in enclosed in single quotes
+    # enclosed in single quotes
     url_formatted = (
-        url + "?" + "format=text&" + "&".join([f"{k}='{v}'" for k, v in params.items()])
+        url
+        + '?'
+        + "format=text&"
+        + '&'.join([f"{k}='{v}'" for k, v in params.items()])
     )
 
     res = requests.get(url_formatted)
@@ -104,32 +117,40 @@ def nasa_horizons(
     fbuf = io.StringIO(res.content.decode())
 
     lines = fbuf.readlines()
-    first_line = lines.index("$$SOE\n") + 1
-    last_line = lines.index("$$EOE\n", first_line)
+    first_line = lines.index('$$SOE\n') + 1
+    last_line = lines.index('$$EOE\n', first_line)
     header_line = lines[first_line - 3]
-    data_str = [header_line] + lines[first_line:last_line]
+    data_str = [header_line] + lines[first_line: last_line]
 
-    data = pd.read_fwf(
-        io.StringIO("\n".join(data_str)), index_col=[0], na_values=["n.a."]
-    )
-    data.index = pd.to_datetime(data.index, format="%Y-%b-%d %H:%M:%S.%f")
-    data.index = data.index.tz_localize("UTC")
+    data = pd.read_fwf(io.StringIO('\n'.join(data_str)),
+                       index_col=[0], na_values=['n.a.'])
+    data.index = pd.to_datetime(data.index, format='%Y-%b-%d %H:%M:%S.%f')
+    data.index = data.index.tz_localize('UTC')
 
     # split columns as several params have a shared header name for two params
-    column_name_split_map = {
-        "R.A._(a-appar)_DEC.": ["right_ascension", "declination"],
-        "Azi____(a-app)___Elev": ["azimuth", "elevation"],
-    }
+    if refraction_correction:
+        column_name_split_map = {
+            # "r" prefix stands for "refracted" aka. refracted/apparent angles
+            'R.A._(r-appar)__DEC': ['apparent_right_ascension', 'apparent_declination'],
+            'Azi____(r-app)___Elev': ['azimuth', 'apparent_elevation'],
+        }
+
+    else:
+        column_name_split_map = {
+            # "a" prefix stands for "airless" aka. unrefracted
+            'R.A._(a-appar)_DEC.': ['right_ascension', 'declination'],
+            'Azi____(a-app)___Elev': ['azimuth', 'elevation'],
+        }
 
     for old_name, new_names in column_name_split_map.items():
-        data[new_names] = data[old_name].str.split(r"\s+", expand=True).astype(float)
+        data[new_names] = \
+            data[old_name].str.split(r'\s+', expand=True).astype(float)
 
     data = data.drop(columns=list(column_name_split_map.keys()))
 
-    data.index.name = "time"
+    data.index.name = 'time'
     try:
-        del data["Unnamed: 1"]
+        del data['Unnamed: 1']
     except KeyError:
         pass
-
     return data
